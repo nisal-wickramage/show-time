11add32d4d374681e95cd18cf8bac76dd034df8f
Nisal Wickramage <nisal@Nisals-MacBook-Pro.local>
2023-01-14 13:43:41 +0800

    Implement reserveration endpoint

--- api/src/main/java/com/showtime/reservation/reservation/ReservationController.java
+++ api/src/main/java/com/showtime/reservation/reservation/ReservationController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ReservationController {
@@ -18,12 +19,21 @@ public class ReservationController {
         this._reservationRepository = reservationRepository;
     }
 
-    @GetMapping("/movies/{movieId}/shows/{showId}/reservations")
-    public ResponseEntity<List<Reservation>> Get(@PathVariable("movieId")long movieId, @PathVariable("showId")long showId) {
+    @GetMapping("/shows/{showId}/reservations")
+    public ResponseEntity<List<Reservation>> GetAll(@PathVariable("showId")long showId) {
         List<Reservation> reservations = _reservationRepository.findByShowId(showId);
         if (reservations.size() == 0) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
         return new ResponseEntity<>(reservations, HttpStatus.OK);
+    }
+
+    @GetMapping("/reservations/{reservationId}")
+    public ResponseEntity<Reservation> Get(@PathVariable("reservationId")long reservationId) {
+        Optional<Reservation> reservation = _reservationRepository.findById(reservationId);
+        if (reservation.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(reservation.get(), HttpStatus.OK);
     }
 }
--- api/src/test/java/com/showtime/reservation/reservation/ReservationControllerTest.java
+++ api/src/test/java/com/showtime/reservation/reservation/ReservationControllerTest.java
@@ -9,10 +9,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -32,7 +29,7 @@ public class ReservationControllerTest {
     public void shouldReturn204WhenNoReservationsFound() throws Exception {
         List<Reservation> reservations = new ArrayList<>();
         when(reservationRepository.findByShowId(1)).thenReturn(reservations);
-        ResultActions result = this.mockMvc.perform(get("/movies/1/shows/1/reservations"));
+        ResultActions result = this.mockMvc.perform(get("/shows/1/reservations"));
         result.andExpect(status().isNoContent());
     }
 
@@ -42,11 +39,33 @@ public class ReservationControllerTest {
         Set<SeatReservation> seats = new HashSet<>();
         seats.add(new SeatReservation('A', 1));
         reservations.add(new Reservation("John", "test@test.com", 1, seats));
-
         when(reservationRepository.findByShowId(1)).thenReturn(reservations);
-        ResultActions result = this.mockMvc.perform(get("/movies/1/shows/1/reservations"));
+
+        ResultActions result = this.mockMvc.perform(get("/shows/1/reservations"));
         result.andExpect(status().isOk());
         result.andExpect(jsonPath("$.length()").value(1));
         result.andExpect(jsonPath("$.[0].seats.length()").value(1));
+    }
+
+    @Test
+    public void shouldReturnSingleReservation() throws Exception {
+        Set<SeatReservation> seats = new HashSet<>();
+        seats.add(new SeatReservation('A', 1));
+        Reservation reservation = new Reservation("John", "test@test.com", 1, seats);
+        when(reservationRepository.findById(Long.valueOf(1))).thenReturn(Optional.of(reservation));
+
+        ResultActions result = this.mockMvc.perform(get("/reservations/1"));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.name").value("John"));
+    }
+
+    @Test
+    public void shouldReturnNoContentWhenReservationNotFound() throws Exception {
+        when(reservationRepository.findById(Long.valueOf(1))).thenReturn(Optional.empty());
+
+        ResultActions result = this.mockMvc.perform(get("/reservations/1"));
+
+        result.andExpect(status().isNoContent());
     }
 }
